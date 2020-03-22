c855d4d3ff16b7722addcfbee3925700d6584e85
Gyugos Péter <gyupetosz@gmail.com>
2020-03-22 18:48:35 +0100

    new

---
+++ Map/Map/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Map.Models;
+
+namespace Map.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
